588d84fff2ce353f09f3668ab9903ed96ca50dac
Wardy <office@dragonsdesign.co.uk>
2021-04-09 19:11:48 +0100

    initial gem build

---
+++ lib/esign_ruby_client.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "esign_ruby_client/version"
+
+module EsignRubyClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/esign_ruby_client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EsignRubyClient do
+  it "has a version number" do
+    expect(EsignRubyClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
